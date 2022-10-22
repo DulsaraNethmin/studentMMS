6b445ba3ac24649140678532ccb8870416e447dc
nethmindulsara <dulsaranethmin@gmail.com>
2022-10-22 17:02:06 +0530

    init project

---
+++ src/main/java/com/example/studentmms/StudentMmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentmms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentMmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studentmms/StudentMmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentmms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentMmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
