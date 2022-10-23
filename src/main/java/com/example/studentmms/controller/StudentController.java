eb13439ffc66d7600f5d670ce509009a296d3f76
nethmindulsara <dulsaranethmin@gmail.com>
2022-10-23 15:32:47 +0530

    add student

--- src/main/java/com/example/studentmms/controller/StudentController.java
+++ src/main/java/com/example/studentmms/controller/StudentController.java
@@ -2,7 +2,10 @@ package com.example.studentmms.controller;
 
 import com.example.studentmms.model.Student;
 import com.example.studentmms.model.Subject;
+import com.example.studentmms.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,10 +16,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class StudentController {
 
+
+    @Autowired
+    StudentService studentService;
     @GetMapping("/student/add")
     public String showAddStudent(Model model){
-        //Subject subject;
-        //model.addAttribute("subject",Subject.);
+
         model.addAttribute("student",new Student());
         return "add-student";
     }
@@ -24,7 +29,16 @@ public class StudentController {
     @PostMapping("/student/add")
     public String addStudent(@ModelAttribute Student student, BindingResult result, Model model){
         System.out.println(student.toString());
+
         model.addAttribute("Student",new Student());
-        return "index";
+        try{
+            Student res=studentService.addStudent(student);
+            return "redirect:";
+            //return new ResponseEntity("Success",HttpStatus.OK);
+        }catch(Exception e){
+            //return new ResponseEntity("Internal Server Error.",HttpStatus.INTERNAL_SERVER_ERROR);
+            return "";
+        }
+        //return "index";
     }
 }
