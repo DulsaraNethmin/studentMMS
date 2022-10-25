10f4e4fe20fcb4ea39fd77d88697fc01fb67241a
nethmindulsara <dulsaranethmin@gmail.com>
2022-10-25 23:30:55 +0530

    show all students table

--- src/main/java/com/example/studentmms/controller/StudentController.java
+++ src/main/java/com/example/studentmms/controller/StudentController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
 
@@ -29,7 +31,10 @@ public class StudentController {
     @GetMapping("/student/show")
     public String showStudentshow(Model model){
         System.out.println("show student");
+        List<Student> students= studentService.getStudentBYTeacher("t-0011");
+        System.out.println(students.size());
         model.addAttribute("student",new Student());
+        model.addAttribute("students",students);
         return "show-student";
     }
 
