866e4cca250e28b4d9d3449679657ce12c5b4040
nethmindulsara <dulsaranethmin@gmail.com>
2022-10-26 10:01:34 +0530

    add result view

--- src/main/java/com/example/studentmms/repository/StudentRepository.java
+++ src/main/java/com/example/studentmms/repository/StudentRepository.java
@@ -1,6 +1,7 @@
 package com.example.studentmms.repository;
 
 import com.example.studentmms.model.Student;
+import com.example.studentmms.model.Subject;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
@@ -10,4 +11,7 @@ public interface StudentRepository extends MongoRepository<Student,String> {
 
     @Query("{'teacher': ?0}")
     List<Student> findByTeacherId(String teacher_id);
+
+    @Query("{'index_no': ?0}")
+    Student getStudentById(String index_no);
 }
